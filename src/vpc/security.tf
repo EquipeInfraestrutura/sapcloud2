ba3b4f8ba4124ae0a5338be442c4f75ba85d40f5
Infraestrutura <equipe.infraestrutura@ias.org.br>
2024-01-09 13:36:38 -0300

    destruindo o ambiente

--- src/vpc/security.tf
+++ src/vpc/security.tf
@@ -52,19 +52,19 @@ resource "aws_security_group" "sg_public" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
   
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["10.11.5.0/24"]
-  }
+  # ingress {
+  #   from_port        = 0
+  #   to_port          = 0
+  #   protocol         = "-1"
+  #   cidr_blocks      = ["10.11.5.0/24"]
+  # }
 
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["172.16.0.0/16"]
-  }
+  # ingress {
+  #   from_port        = 0
+  #   to_port          = 0
+  #   protocol         = "-1"
+  #   cidr_blocks      = ["172.16.0.0/16"]
+  # }
   
   #ingress {
    # from_port        = 3389
