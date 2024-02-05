61b9059cb836e1ff1467aaa2f9bff228fe03634e
Infraestrutura <equipe.infraestrutura@ias.org.br>
2024-02-05 15:45:22 -0300

    terraform plan

--- src/main.tf
+++ src/main.tf
@@ -1,13 +1,43 @@
-module "vpc" {
-  source          = "./vpc"
+# Create a EC2 Instance (Windows Server 2022)
+resource "aws_instance" "windows_client" {
+  count                  = var.instance_count
+  instance_type          = var.instance_type_client
+  ami                    = var.amiclient_id
+  key_name               = var.key_pair
+  vpc_security_group_ids = var.sg_public
+  subnet_id              = var.public_subnet
+
+  tags = {
+    Name = "SAP_Client_Windows-${count.index + 1}"
+  }
+
+  root_block_device {
+    volume_size = var.volume_size
+  }
 }
 
-module "ec2" {
-  source                = "./ec2"
-  
-  vpc_id                 = module.vpc.vpc_id
-  sg_public              = [module.vpc.sg_public]
-  public_subnet          = module.vpc.public_subnet
-  sg_private             = [module.vpc.sg_private]
-  private_subnet         = module.vpc.private_subnet
+# Create and assosiate an Elastic IP to client Instance
+resource "aws_eip" "eip_pub" {
+  count    = length(var.instance_count)
+  instance = aws_instance.windows_client[count.index].id
 }
+
+# Create a EC2 Instance (Windows Server with SQL Server 2022)
+resource "aws_instance" "windows_sql" {
+  count                  = var.instance_count
+  instance_type          = var.instance_type_sql
+  ami                    = var.amisql_id
+  key_name               = var.key_pair
+  vpc_security_group_ids = var.sg_private
+  subnet_id              = var.private_subnet
+
+  tags = {
+    Name = "SAP_SQL_Windows-${count.index + 1}"
+  }
+
+  root_block_device {
+    volume_size = var.volume_size
+  }
+}
+
+
